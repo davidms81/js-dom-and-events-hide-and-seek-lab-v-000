0b8d3b1ec50a71a15b342e78c559add21c1868ff
David Schoenholtz <dmschoenholtz@gmail.com>
2019-08-06 17:14:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,9 +6,16 @@ function nestedTarget(){
   return document.querySelector('#nested .target')
 }
 
-function increaseRankBy(n){
-  const ranked = document.querySelectorAll('ul.unranked-list li')
-  
+function increaseRankBy( n ) {
+  const rankedLists = document.querySelectorAll( '.ranked-list' )
+
+  for ( let i = 0, l = rankedLists.length; i < l; i++ ) {
+    let children = rankedLists[ i ].children
+
+    for ( let j = 0, k = children.length; j < k; j++ ) {
+      children[ j ].innerHTML = parseInt( children[ j ].innerHTML ) + n
+    }
+  }
 }
 
 function deepestChild(){
